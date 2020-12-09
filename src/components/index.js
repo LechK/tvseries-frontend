fb756f30ebc1a72c230c754127ff4da5d690c562
LechKuznecov <46899047+LechKuznecov@users.noreply.github.com>
2020-12-09 20:41:47 +0200

    added burger and menu  components export

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,5 @@ export { default as Section } from "./Section/Section";
 export { default as Loading } from "./Loading/Loading";
 export { default as TextField } from "./TextField/TextField";
 export { default as Notification } from "./Notification/Notification";
+export { default as Burger } from "./Burger/Burger";
+export { default as Menu } from "./Menu/Menu";
