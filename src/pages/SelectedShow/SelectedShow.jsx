59ce1e6523e4a5a0b37605fbb8414c6a504f2e9b
LechKuznecov <46899047+LechKuznecov@users.noreply.github.com>
2020-12-15 20:43:12 +0200

    selected show progression

--- src/pages/SelectedShow/SelectedShow.jsx
+++ src/pages/SelectedShow/SelectedShow.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Section } from "../../components";
 import * as S from "./SelectedShow.style";
 
 function SelectedShow() {
@@ -17,7 +18,30 @@ function SelectedShow() {
     <>
       {show && show.id}
       <S.ShowPage>
-        <S.Wallpaper src="https://walter.trakt.tv/images/shows/000/139/960/fanarts/full/cae156577f.jpg.webp" />
+        <Section>
+          <S.Heading>The Boys</S.Heading>
+        </Section>
+        <S.Wallpaper background="https://walter.trakt.tv/images/shows/000/139/960/fanarts/full/cae156577f.jpg.webp" />
+        <Section>
+          <S.H5>
+            <S.Span>CREATOR:</S.Span> Simon Rabbits
+          </S.H5>
+          <S.H5>
+            <S.Span>Premiered:</S.Span> 2019
+          </S.H5>
+          <S.H5>
+            <S.Span> Network:</S.Span> HBO
+          </S.H5>
+          <S.H4>
+            Description: Lorem ipsum dolor sit amet consectetur adipisicing
+            elit. Sit deleniti at neque accusamus ipsa harum repellendus, nisi
+            error sapiente quas laborum exercitationem dolorem quia repudiandae
+            error sapiente quas laborum exercitationem dolorem quia repudiandae
+            error sapiente quas laborum exercitationem dolorem quia repudiandae
+            error sapiente quas laborum exercitationem dolorem quia repudiandae
+          </S.H4>
+          <img src="" alt="" />
+        </Section>
       </S.ShowPage>
     </>
   );
--- src/pages/SelectedShow/SelectedShow.style.js
+++ src/pages/SelectedShow/SelectedShow.style.js
@@ -1,12 +1,63 @@
 import styled from "styled-components";
 
 export const ShowPage = styled.div`
-  background-color: #eee;
+  background: ${({ theme }) => theme.darkGrey};
+  && Section {
+    background: ${({ theme }) => theme.darkGrey};
+    padding: 0 1em;
+  }
 `;
 
 export const Wallpaper = styled.img`
   width: 100%;
-  height: calc(100vh - 113px);
-  object-position: top;
-  object-fit: cover;
+  height: 60vh;
+  background-image: url(${(props) => props.background});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position-y: 15%;
+  @media (max-width: ${({ theme }) => theme.medium}) {
+    height: 50vh;
+    background-position: center;
+  }
+  @media (max-width: ${({ theme }) => theme.mobile}) {
+    height: 40vh;
+    background-position: center;
+  }
+`;
+
+export const Heading = styled.h1`
+  color: whitesmoke;
+  margin-top: 0;
+  font-size: 3em;
+  @media (max-width: ${({ theme }) => theme.medium}) {
+    font-size: 2em;
+    left: 0.7em;
+    top: -0.2em;
+  }
+  @media (max-width: ${({ theme }) => theme.mobile}) {
+    font-size: 1.5em;
+    left: 0.7em;
+    top: -0.2em;
+  }
+`;
+
+export const H5 = styled.h5`
+  color: ${({ theme }) => theme.lightGrey};
+  font-weight: bold;
+  margin: 0.7em 0;
+`;
+export const H4 = styled.h4`
+  color: ${({ theme }) => theme.lightGrey};
+  font-weight: bold;
+  padding-top: 0.5em;
+  margin: 0.7em 0;
+  @media (max-width: ${({ theme }) => theme.mobile}) {
+    font-size: small;
+  }
+`;
+
+export const Span = styled.span`
+  color: ${({ theme }) => theme.magenta};
+  font-weight: normal;
+  font-size: large;
 `;
