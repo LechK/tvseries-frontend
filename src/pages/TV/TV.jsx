6dc03a84944e17d170d300d088d97ecb76205aab
LechKuznecov <46899047+LechKuznecov@users.noreply.github.com>
2020-12-15 02:00:20 +0200

    TV show fetch from db

--- src/pages/TV/TV.jsx
+++ src/pages/TV/TV.jsx
@@ -1,36 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import { Section, ShowBox } from "../../components";
 import * as S from "./TV.style";
 
 function TV() {
+  const [series, setSeries] = useState();
+  const history = useHistory();
+
+  useEffect(() => {
+    fetch(`http://localhost:8080/tvseries`)
+      .then((res) => res.json())
+      .then((data) => setSeries(data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <S.TV>
       <Section>
-        <ShowBox
-          src="https://walter.trakt.tv/images/shows/000/001/390/fanarts/full/76d5df8aed.jpg.webp"
-          title="Game of Thrones"
-          year="2011"
-        />
-        <ShowBox
-          src="https://walter.trakt.tv/images/shows/000/001/390/fanarts/full/76d5df8aed.jpg.webp"
-          title="Game of Thrones"
-          year="2011"
-        />
-        <ShowBox
-          src="https://walter.trakt.tv/images/shows/000/001/390/fanarts/full/76d5df8aed.jpg.webp"
-          title="Game of Thrones"
-          year="2011"
-        />
-        <ShowBox
-          src="https://walter.trakt.tv/images/shows/000/001/390/fanarts/full/76d5df8aed.jpg.webp"
-          title="Game of Thrones"
-          year="2011"
-        />
-        <ShowBox
-          src="https://walter.trakt.tv/images/shows/000/001/390/fanarts/full/76d5df8aed.jpg.webp"
-          title="Game of Thrones"
-          year="2011"
-        />
+        {series &&
+          series.map((show) => {
+            return (
+              <ShowBox
+                key={show.id}
+                src={show.wallpaper}
+                title={show.title}
+                year={show.premiere}
+                handleClick={() => history.push(`/selectedShow/${show.id}`)}
+              />
+            );
+          })}
       </Section>
     </S.TV>
   );
