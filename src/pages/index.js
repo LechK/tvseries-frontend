6c8ea7c5c5ae556aa11092fee28a6d873fb4d785
LechKuznecov <46899047+LechKuznecov@users.noreply.github.com>
2020-12-18 22:30:51 +0200

    tv series changed to shows

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 export { default as Home } from "./Home/Home.jsx";
-export { default as TV } from "./TV/TV.jsx";
+export { default as Shows } from "./Shows/Shows.jsx";
 export { default as AddSeries } from "./Add/AddSeries.jsx";
 export { default as Login } from "./Login/Login.jsx";
 export { default as Register } from "./Register/Register.jsx";
 export { default as SelectedShow } from "./SelectedShow/SelectedShow.jsx";
+export { default as Episode } from "./Episode/Episode.jsx";
